a548ecc9435dfc3ebda5b3bc3533e1dca75c538e
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-07-30 15:30:24 +0000

    Create function to handle lives and add end game conditions

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import random
 import string
 from words import words
 
+alphabet = set(string.ascii_uppercase)
+used_letters = set() # stores letters already guessed
+lives = 6 # equivalent to number of strokes in hangman image 
+
+
 def get_word(words):
     """
     Returns random word from words list
@@ -11,6 +16,8 @@ def get_word(words):
 
     return word
 
+word = get_word(words)
+word_letters = set(word)
 
 def get_user_input():
     """
@@ -20,6 +27,14 @@ def get_user_input():
 
     return player_letter
 
+def handle_lives():
+    """
+    Decrements lives for each wrong guess
+    """
+    global lives
+    lives = lives - 1 
+    print(f"Wrong letter! You have {lives} lives left.")
+    
 
 def check_letter():
     """
@@ -27,7 +42,7 @@ def check_letter():
     and if letter is in word
     """
     player_letter = get_user_input() # current guessed letter
-
+    
     # adds guessed letter to used letters set
     if player_letter in alphabet - used_letters:
         used_letters.add(player_letter)
@@ -36,6 +51,9 @@ def check_letter():
         if player_letter in word_letters:
             word_letters.remove(player_letter)
             print("correct letter")
+        else:
+            handle_lives()
+
     # tells user to guess again if letter is already guessed
     elif player_letter in used_letters:
         print("Sorry, you've already guessed this one. Try a different letter!")
@@ -56,21 +74,23 @@ def run_game():
     """
     Runs a loop to determine when game is finished
     """
-    while len(word_letters) > 0:
-        print(f"You've already used these letter: {' '.join(used_letters)}")
+    print(word) #remove later
+    while len(word_letters) > 0 and lives > 0:
+        print(f"You've guessed these letters so far: {' '.join(used_letters)}")
 
         display_word()
 
         check_letter()
 
-        if len(word_letters) == 0:
+        if lives == 0:
+            print(f"Sorry, you're dangling 😢 The word was {word}")
+            break
+        elif len(word_letters) == 0:
+            print("🎉 Well done! You guessed the whole word 🎉")
             break
 
 
-word = get_word(words)
-word_letters = set(word)
-alphabet = set(string.ascii_uppercase)
-used_letters = set() # stores letters already guessed
+
 
 
 def main():
